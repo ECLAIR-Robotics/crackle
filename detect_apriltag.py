65ad60810f37834c64985d4bb25168d5e5b4409d
Tony Dinh <tonydinh442@gmail.com>
2024-03-05 19:32:45 -0600

    Put homography inside class

--- detect_apriltag.py
+++ detect_apriltag.py
@@ -2,68 +2,23 @@ import robotpy_apriltag
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
-tag = robotpy_apriltag.AprilTagDetector()
-tag.addFamily("tag36h11", 3)
-
-WIDTH = 640
-HEIGHT = 480
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame = cv2.resize(frame, (640, 480))
-
-    l: robotpy_apriltag.AprilTagDetection = tag.detect(frame)
-    if l is not None:
-        for i in l:
-            # print(i.getCenter())
-            point = i.getCenter()
-            x: int = int(point.x)
-            y: int = int(point.y)
-
-            point_tl = (int(i.getCorner(0).x), int(i.getCorner(0).y))
-            point_br = (int(i.getCorner(2).x), int(i.getCorner(2).y))
-
-            cv2.rectangle(frame, point_tl, point_br, (0, 0, 255), thickness=2)
-            
-            # points = i.getCorners()
-            # rect = cv2.minAreaRect(points)
-            # box = cv2.boxPoints(rect)
-            
-            # cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
-            
-            cv2.line(frame, (x, 0), (x, HEIGHT), (0, 255, 0), thickness=2)
-            cv2.line(frame, (0, y), (WIDTH, y), (0, 255, 0), thickness=2)
-
-    # Display the frame
-    cv2.imshow("Camera Feed", frame)
-
-    # Break the loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-# Release the capture object and close the window
-cap.release()
-cv2.destroyAllWindows()
-
-class AprilTage:
+class AprilTags:
     def __init__(self) -> None:
-        self.width = 640
-        self.height = 480
+        self.WIDTH = 640
+        self.HEIGHT = 480
         
         self.cap = cv2.VideoCapture(0)
         self.tag = robotpy_apriltag.AprilTagDetector()
-        tag.addFamily("tag36h11", 3)
+        self.tag.addFamily("tag36h11", 3)
         
-    def detector():
+    def detector(self):
         while True:
              # Capture frame-by-frame
-             ret, frame = cap.read()
+             ret, frame = self.cap.read()
              frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
              frame = cv2.resize(frame, (640, 480))
 
-             l: robotpy_apriltag.AprilTagDetection = tag.detect(frame)
+             l: robotpy_apriltag.AprilTagDetection = self.tag.detect(frame)
              if l is not None:
                 for i in l:
                     # print(i.getCenter())
@@ -82,8 +37,8 @@ class AprilTage:
                         
                      # cv2.drawContours(frame, [box], 0, (0, 255, 0), 2)
                         
-                    cv2.line(frame, (x, 0), (x, HEIGHT), (0, 255, 0), thickness=2)
-                    cv2.line(frame, (0, y), (WIDTH, y), (0, 255, 0), thickness=2)
+                    cv2.line(frame, (x, 0), (x, self.HEIGHT), (0, 255, 0), thickness=2)
+                    cv2.line(frame, (0, y), (self.WIDTH, y), (0, 255, 0), thickness=2)
 
                     # Display the frame
                     cv2.imshow("Camera Feed", frame)
@@ -91,6 +46,79 @@ class AprilTage:
                     # Break the loop if 'q' key is pressed
                     if cv2.waitKey(1) & 0xFF == ord("q"):
                         break
-        
-    
+
+        # Release the capture object and close the window
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def getHomography(self):
+        WIDTH_OUTPUT = 150
+        HEIGHT_OUTPUT = 150
+
+        while True:
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame = cv2.resize(frame, (640, 480))
+            warped_img = frame
+            l: robotpy_apriltag.AprilTagDetection = self.tag.detect(frame)
+            if l is not None:
+                for i in l:
+                    # print(i.getCenter())
+                    point = i.getCenter()
+                    x: int = int(point.x)
+                    y: int = int(point.y)   
+
+                    points = [(0, 0), (0, 0), (0, 0), (0, 0)]
+                    colors_list = [(0, 0, 255), (0, 255, 255), (255, 0, 0), (255, 255, 255)]
+                    for j in range(len(points)):
+                        points[j] = (int(i.getCorner(j).x), int(i.getCorner(j).y))   
+                    
+                    for j in range(4):
+                        cv2.circle(frame, points[j], 2, (50, 0, 0), thickness=j)
+                    """"
+                    A3 ----------- B2
+                    |             |
+                    |             |
+                    C0 ----------- D1
+                    
+                    """
+                    # Calculate the width and height of the april tag to calculate homography
+                    width_AB = np.sqrt(((points[2][0] - points[3][0])**2) + ((points[2][1] - points[3][1])**2))
+                    width_CD = np.sqrt(((points[1][0] - points[0][0])**2) + ((points[1][1] - points[0][1])**2))
+                    # width = max(width_AB, width_CD)
+                    width = WIDTH_OUTPUT
+                    
+                    height_BD = np.sqrt(((points[2][0] - points[1][0])**2) + ((points[2][1] - points[1][1])**2))
+                    height_AC = np.sqrt(((points[3][0] - points[0][0])**2) + ((points[3][1] - points[0][1])**2))
+                    
+                    # height = max(height_BD, height_AC)
+                    height = HEIGHT_OUTPUT
+                    input_pts = np.float32([points[3], points[0], points[1], points[2]])
+                    output_pts = np.float32([[0, 0],
+                                [0, height - 1],
+                                [width - 1, height - 1],
+                                [width - 1, 0]])
+                    M = cv2.getPerspectiveTransform(input_pts, output_pts)
+                    print("M: ", M)
+                    warped_img = cv2.warpPerspective(frame, M, (int(width), int(height)), flags=cv2.INTER_LINEAR)
+                    
+                    cv2.line(frame, (x, 0), (x, self.HEIGHT), (0, 255, 0), thickness=2)
+                    cv2.line(frame, (0, y), (self.WIDTH, y), (0, 255, 0), thickness=2)
             
+
+            # Display the frame
+            cv2.imshow("Camera Feed", frame)
+            cv2.imshow("Flat image", warped_img)
+
+            # Break the loop if 'q' key is pressed
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+
+        # Release the capture object and close the window
+        self.cap.release()
+        cv2.destroyAllWindows()
+    
+if __name__ == "__main__":
+    april_tag_detector = AprilTags()
+    april_tag_detector.getHomography()
