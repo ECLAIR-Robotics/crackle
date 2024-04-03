51fc8206a0cd7c814a2185299a4701935986f865
rhubarbhill <90863727+rhubarbhill@users.noreply.github.com>
2024-04-02 19:42:25 -0500

    Small adjustments to ros2

--- ros2_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
+++ ros2_ws/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -26,7 +26,7 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(Float64MultiArray, 'topic', 10)
+        self.publisher_ = self.create_publisher(Float64MultiArray, '/topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
