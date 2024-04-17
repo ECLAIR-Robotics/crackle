f4bda70d76e7b6f9a741e25e257f21f39b173bab
rhubarbhill <90863727+rhubarbhill@users.noreply.github.com>
2024-04-16 19:24:05 -0500

    Added photo package folder

---
+++ crackle_ws/src/pkg_photo/pkg_photo/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from pkg_photo.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ crackle_ws/src/pkg_photo/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'pkg_photo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rhubarbhill',
+    maintainer_email='rhubarbhill@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = pkg_photo.my_node:main'
+        ],
+    },
+)
