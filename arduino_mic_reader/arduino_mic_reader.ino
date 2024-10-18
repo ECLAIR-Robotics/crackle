6d299eb263c16bc4511466a417077f86da4e5378
Yash Karandikar <nerdstep710@gmail.com>
2024-10-17 23:04:50 -0500

    Update arduino code

--- arduino_mic_reader/arduino_mic_reader.ino
+++ arduino_mic_reader/arduino_mic_reader.ino
@@ -1,85 +1,144 @@
 #include <Arduino.h>
 
-const int voltage = 330; // 3.3 volts
+const int VOLTAGE = 330; // 3.3 volts
 
-const int lower_bound = 150;
-const int higher_bound = 300;
+// Mic Setup
+const int LEFT_MIC = A0;
+const int RIGHT_MIC = A15;
 
-const int MIC_RIGHT = A0;
-const int MIC_LEFT = A15;
+// Avg calculator setup
+const double FIRST_REGISTER_RATIO = 1.75;  // how far over sx first heard sound must be
+const double SECOND_REGISTER_RATIO = 1.25; // how far over sx second heard sound must be
+double leftAvg = 0;                        // avg heard sound of leftMic, update as progresses
+double rightAvg = 0;                       // avg heard sound of rightMic, update as progresses
+const int NUM_READS_PER_AVG = 150;         // how many reads must be made to update sound average
+double leftHistory[NUM_READS_PER_AVG];     // history for next avg calc on left mic
+double rightHistory[NUM_READS_PER_AVG];    // history for next avg calc on right mic
+int numReadsMade = 0;                      // how many reads have been made to this point
 
-void setup() {
-  Serial.begin(19200);
-  ADCSRA = (ADCSRA & 0xF8) | 0x04; // Prescaler of 16
-  ADCSRA &= ~(1 << ADATE); // Disable auto triggering
-  pinMode(MIC_RIGHT, INPUT);
-  pinMode(MIC_LEFT, INPUT);
+// rate at which signal is sent over USB
+const int BAUDRATE = 9600;
+
+const unsigned long WAIT_TIME = 500; // how long we will freeze the program to wait for sound
+
+void setup()
+{
+    Serial.begin(BAUDRATE);
+    pinMode(LEFT_MIC, INPUT);
+    pinMode(RIGHT_MIC, INPUT);
+    calibrateMics();
+    ADCSRA = (ADCSRA & 0xF8) | 0x04; // Set Analogue Read Rate
+    ADCSRA &= ~(1 << ADATE); // Disable auto triggering
 }
 
-bool left_mic_listening = false;
-int left_max_read = 0;
-long left_max_time_stamp;
-int left_last_read;
-int left_second_last_read;
-
-bool right_mic_listening = false;
-int right_max_read = 0;
-long right_max_time_stamp;
-int right_last_read;
-int right_second_last_read;
-
-int absolute(int x) {
-  if (x < 0) {
-    return -x;
-  }
-  return x;
+// get an initial average to base everything else off of
+// ideally run this with average background noise and no distrubrance you would want detected
+void calibrateMics()
+{
+    for (int i = 0; i < NUM_READS_PER_AVG; i++)
+    {
+        double leftSound = analogRead(LEFT_MIC);
+        double rightSound = analogRead(RIGHT_MIC);
+        updateAverages(leftSound, rightSound);
+    }
 }
 
-void loop() {
-  int left_read = absolute(analogRead(MIC_LEFT)-voltage);
-  if (!left_mic_listening && left_read > higher_bound) {
-    left_mic_listening = true;
-    left_max_read = left_read;
-    left_max_time_stamp = micros();
-  }
-  if (left_mic_listening) {
-    if (left_read > left_max_read) {
-      left_max_read = left_read;
-      left_max_time_stamp = micros();
-    } else if (left_last_read > left_second_last_read && left_read < left_last_read && left_last_read < lower_bound) {
-      left_mic_listening = false;
+// calculates avg of values in an array
+double getAverage(double vals[])
+{
+    double avg = 0;
+    for (int i = 0; i < sizeof(vals); i++)
+    {
+        avg += vals[i];
     }
-  }
-  left_second_last_read = left_last_read;
-  left_last_read = left_read;
+    return avg / sizeof(vals);
+}
 
-  int right_read = absolute(analogRead(MIC_RIGHT)-voltage);
-  if (!right_mic_listening && right_read > higher_bound) {
-    right_mic_listening = true;
-    right_max_read = right_read;
-    right_max_time_stamp = micros();
-  }
-  if (right_mic_listening) {
-    if (right_read > right_max_read) {
-      right_max_read = right_read;
-      right_max_time_stamp = micros();
-    } else if (right_last_read > right_second_last_read && right_read < right_last_read && right_read < lower_bound) {
-      right_mic_listening = false;
+// reset the historys
+void resetHist()
+{
+    for (int i = 0; i < NUM_READS_PER_AVG; i++)
+    {
+        leftHistory[i] = 0;
+        rightHistory[i] = 0;
     }
-  }
-  right_second_last_read = right_last_read;
-  right_last_read = right_read;
+}
 
-  // Serial.print("Mic_Left:");
-  // Serial.print(analogRead(MIC_LEFT));
-  // Serial.print(", ");
+// updates left and right mic averages
+void updateAverages(double leftVal, double rightVal)
+{
+    // calculate avgs / reset counter for cycle: takes many computations so do
+    // infrequently. Frequency determined by NUM_READS_PER_AVG. NOTE: lower frequency
+    // means more computations each time this happens but should be neg. increase
+    if (numReadsMade == NUM_READS_PER_AVG)
+    {
+        leftAvg = getAverage(leftHistory);
+        rightAvg = getAverage(rightHistory);
+        resetHist();
+        numReadsMade = 0;
+    }
 
-  // Serial.print("Mic_Right:");
-  // Serial.println(analogRead(MIC_RIGHT));
+    // add vals to next calculation of avg
+    leftHistory[numReadsMade] = leftVal;
+    rightHistory[numReadsMade] = rightVal;
+    numReadsMade++;
+}
 
-  if (!left_mic_listening && !right_mic_listening && left_max_time_stamp != 0 && right_max_time_stamp != 0 && absolute(left_max_time_stamp-right_max_time_stamp) < 1000) {
-    Serial.println(left_max_time_stamp-right_max_time_stamp);
-    left_max_time_stamp = 0;
-    right_max_time_stamp = 0;
-  }
+// each time we are looking for a deltaTime
+// return time delta if applicable
+//      positive val if right first
+//      negative val if left first
+//      WAIT_TIME + 1 if trash data
+unsigned long getAudioDelta()
+{
+    unsigned long startTime = micros();
+    while (micros() - startTime < WAIT_TIME)
+    {
+        // right mic heard first
+        if (analogRead(RIGHT_MIC) > rightAvg * FIRST_REGISTER_RATIO)
+        {
+            unsigned long firstRead = micros();
+            while (int(micros() - firstRead) < WAIT_TIME)
+            {
+                if (analogRead(LEFT_MIC) > leftAvg * SECOND_REGISTER_RATIO)
+                {
+                    return micros() - firstRead;
+                }
+            }
+            return WAIT_TIME + 1;
+        }
+
+        // left mic heard first
+        if (analogRead(LEFT_MIC) > leftAvg * FIRST_REGISTER_RATIO)
+        {
+            unsigned long firstRead = micros();
+            while (micros() - firstRead < WAIT_TIME)
+            {
+                if (analogRead(RIGHT_MIC) > rightAvg * SECOND_REGISTER_RATIO)
+                {
+                    return micros() - firstRead;
+                }
+            }
+            return WAIT_TIME + 1;
+        }
+    }
+
+    return WAIT_TIME + 1;
+}
+
+void loop()
+{
+    // get the delta
+    unsigned long delta = getAudioDelta();
+
+    // only waste time with serial when data is useful
+    if (delta != WAIT_TIME + 1 || delta == 40)
+    {
+        Serial.println(delta - 40);
+    }
+
+    // take a sample for avg and update if needed
+    double leftSound = analogRead(LEFT_MIC);
+    double rightSound = analogRead(RIGHT_MIC);
+    updateAverages(leftSound, rightSound);
 }
