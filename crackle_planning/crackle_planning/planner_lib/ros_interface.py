2945c098e24193277befd70890a7ef7b4750a66f
Tanay Garg <tanay.garg@utexas.edu>
2024-10-31 19:43:23 -0500

    added planning:

--- crackle_planning/crackle_planning/planner_lib/ros_interface.py
+++ crackle_planning/crackle_planning/planner_lib/ros_interface.py
@@ -2,7 +2,8 @@ import rclpy
 from rclpy.node import Node
 
 class RosInterface:
-    def __init__(self, node : Node, node_name):
-        super().__init__(node_name)
+    def __init__(self, node : Node):
+        pass
+
 
     
