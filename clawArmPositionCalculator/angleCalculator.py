922bc1ca441d53fd0cfd280c6bdd2c5e882f8802
MPKaronde <karonde.manav@gmail.com>
2025-02-06 22:15:54 -0600

    initialcommit

---
+++ clawArmPositionCalculator/angleCalculator.py
@@ -0,0 +1,12 @@
+# all units in mm and radians
+
+
+# represents each claw arm
+class ClawArm:
+
+    # init
+    def __init__(self, x_cord, y_cord, z_cord, rads):
+        self.x = x_cord
+        self.y = y_cord
+        self.z = z_cord
+        self.rads = rads
