30fa579814ef070aad65ab5ad23ff4dac1372ef7
leo-witzke <leowitzke00@gmail.com>
2025-04-03 21:52:39 -0500

    cooking
    Co-authored-by: Yash Karandikar <karx1@users.noreply.github.com>
    Co-authored-by: ManasAgrawal05 <ManasAgrawal05@users.noreply.github.com>

--- clawArmPositionCalculator/ClawArm/src/main.cpp
+++ clawArmPositionCalculator/ClawArm/src/main.cpp
@@ -1,209 +1,132 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
-<<<<<<< HEAD
 #include <iostream>
 #include <vector>
 #include <stdexcept>
-=======
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
+#include <thread>
 
-// servo constants
-#define SERVO_HERTZ 50
-#define SERVO_MIN 500
-#define SERVO_MAX 2400
+// Maximum value of a 16-bit signed integer
+#define INT_MAX_VAL 32767
+
+#define RESISTANCE_THRESHOLD 1780
+
 #define NUMBER_OF_SERVOS 3
-<<<<<<< HEAD
 
-// TODO: set real min and max constants
-// IMPORTANT ::: IMPORTANT
-// PLEASE DO THIS OR EVERYTHING WILL BLOW UP!!!!!
-// IMPORTANT FIRST STEP AS SOON AS YOU CAN MAKE THINGS RUN
-#define MIN_DEGREE 30
-#define MAX_DEGREE 70
-
-=======
-
-// TODO: set real min and max constants
-// IMPORTANT ::: IMPORTANT
-// PLEASE DO THIS OR EVERYTHING WILL BLOW UP!!!!!
-// IMPORTANT FIRST STEP AS SOON AS YOU CAN MAKE THINGS RUN
 #define MIN_DEGREE 0
 #define MAX_DEGREE 180
 
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
 // serial parameters
 #define BAUD_RATE 9600
 
-// Class for the claw fingers
-<<<<<<< HEAD
-class KrabbyPatty
+// class for claw fingers
+class ClawFinger
 {
 public:
     Servo servo;
     int location;
     int sensor;
-    KrabbyPatty(int servoPin, int sensorPin)
+    ClawFinger(int servoPin, int sensorPin)
     {
         servo.attach(servoPin);
         location = 0;
         pinMode(sensorPin, INPUT);
         sensor = sensorPin;
     }
-    KrabbyPatty() {}
-=======
-class KrabbyPatty {
-    public:
-        Servo servo;
-        int location;
-        int sensor;
-        KrabbyPatty(int servoPin, int sensorPin)
-        {
-            servo.attach(servoPin);
-            location = 0;
-            pinMode(sensorPin, INPUT);
-            sensor = sensorPin;
-        }
-        KrabbyPatty() {}
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
+    ClawFinger() {}
 };
 
-// servo list
-KrabbyPatty krabbyPattyLookUp[NUMBER_OF_SERVOS] = {
-    KrabbyPatty(13, 12),
-    KrabbyPatty(4, 5),
-<<<<<<< HEAD
-    KrabbyPatty(18, 19)};
-=======
-    KrabbyPatty(18, 19)
-};
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
+// list of claw fingers
+ClawFinger clawFingerLookup[NUMBER_OF_SERVOS] = {
+    ClawFinger(13, 12),
+    ClawFinger(27, 14),
+    ClawFinger(26, 25)};
 
-// zero the claws
-bool zero()
-{
-    bool servosZeroed = true;
-    for (int i = 0; i < NUMBER_OF_SERVOS; i++)
-    {
-<<<<<<< HEAD
-        if (krabbyPattyLookUp[i].servo.attached())
-        {
-            krabbyPattyLookUp[i].servo.write(MIN_DEGREE);
-        }
-        else
-        {
-=======
-        if (krabbyPattyLookUp[i].servo.attached()) {
-            krabbyPattyLookUp[i].servo.write(MIN_DEGREE);
-        } else {
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
-            servosZeroed = false;
-        }
-    }
-    return servosZeroed;
-}
 
-// setup
-void setup()
-{
-    Serial.begin(BAUD_RATE);
-
-    zero();
-}
-
-// read current serial message
-// returns string on current serial line
-<<<<<<< HEAD
-String readSerial()
-{
-    String command = Serial.readString();
-    return command;
-}
-
-// send a message over serial
-// returns true if message successfuly sent
-bool writeSerial(String message)
-{
-    Serial.println(message);
-}
-=======
-// String readSerial()
-// {
-//     String command = Serial.readString();
-//     return command;
-// }
-
-// // send a message over serial
-// // returns true if message successfuly sent
-// bool writeSerial(String message)
-// {
-//     Serial.println(message);
-// }
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
-
-// move the servo
-// take in servo number and degree
+// moves an individual servo w/out reacting to resistance
 bool moveServo(int servoID, int degree)
 {
-    if (servoID < 0 || servoID >= NUMBER_OF_SERVOS || degree < MIN_DEGREE || degree > MAX_DEGREE) {
+    // degree out of bounds
+    if (servoID < 0 || servoID >= NUMBER_OF_SERVOS || degree < MIN_DEGREE || degree > MAX_DEGREE)
+    {
         return false;
     }
-
-<<<<<<< HEAD
-    // KrabbyPatty krabbyPatty = krabbyPattyLookUp[servoID];
-    // krabbyPatty.servo.write(degree);
-    // krabbyPatty.location = degree;
-
-=======
-    krabbyPattyLookUp[servoID].servo.write(degree);
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
+    clawFingerLookup[servoID].servo.write(degree);
+    clawFingerLookup[servoID].location = degree;
     return true;
 }
 
-// get the largest of the three ints
-// int largest(int servoOneAmt, int servoTwoAmt, int servoThreeAmt)
-// {
-//     int largest = servoOneAmt;
-//     if (servoTwoAmt > largest)
-//     {
-//         largest = servoTwoAmt;
-//     }
-//     if (servoThreeAmt > largest)
-//     {
-//         largest = servoThreeAmt;
-//     }
-//     return largest;
-// }
-
-<<<<<<< HEAD
-=======
-// close servo
-// TODO: implement logic to run until ampMeter spike
-int closeServo(int servoOneAmt, int servoTwoAmt, int servoThreeAmt)
+// TODO: code with a usable zero routine
+bool zero()
 {
-    // int largestServo = largest(servoOneAmt, servoTwoAmt, servoThreeAmt);
-    // int servoOneReadings[20];
-    // int servoTwoReadings[20];
-    // int servoThreeReadings[20];
-
-    // // increment each servo by one up until closed
-    // for (int i = 0; i < largestServo; i++)
-    // {
-    //     // if (krabbyPattyLookUp[1].location < servoOneAmt)
-    //     // {
-    //     //     moveServo(1, 1);
-    //     // }
-    //     // if (krabbyPattyLookUp[2].location < servoTwoAmt)
-    //     // {
-    //     //     moveServo(2, 1);
-    //     // }
-    //     // if (krabbyPattyLookUp[3].location < servoThreeAmt)
-    //     // {
-    //     //     moveServo(3, 1);
-    //     // }
-    // }
+    for(int i = 0; i < NUMBER_OF_SERVOS; i++)
+    {
+        moveServo(i, 0);
+    }
+    return true;
 }
 
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
+int sign(int x) {
+    if (x > 0) return 1;
+    if (x < 0) return -1;
+    return 0;
+}
+
+int moveServoToResistanceRecursive(int servoID, int degree, int finalDegree)
+{
+    if (degree == finalDegree || degree < MIN_DEGREE || degree > MAX_DEGREE) {
+        return clawFingerLookup[servoID].location;
+    }
+    int numberSensorReadings = 5;
+    int sensorReadingSum = 0;
+    for (int i = 0; i < numberSensorReadings; i++) {
+        sensorReadingSum += analogRead(clawFingerLookup[servoID].sensor);
+    } 
+    int averageSensorReading = (int)(sensorReadingSum) / numberSensorReadings;
+    if (averageSensorReading < RESISTANCE_THRESHOLD) {
+        return clawFingerLookup[servoID].location;
+    }
+
+    bool servoMoved = moveServo(servoID, degree); 
+    if (servoMoved == false) {
+        return clawFingerLookup[servoID].location;
+    }
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    return moveServoToResistanceRecursive(servoID, degree + sign(finalDegree-clawFingerLookup[servoID].location)*1, finalDegree);
+}
+
+int moveServoToResistance(int servoID, int finalDegree) {
+    if (clawFingerLookup[servoID].location == finalDegree) {
+        return finalDegree;
+    }
+    return moveServoToResistanceRecursive(servoID, clawFingerLookup[servoID].location + sign(finalDegree-clawFingerLookup[servoID].location)*1, finalDegree);
+}
+
+// moveServoToResistance to 0 for all servos
+bool closeClaw()
+{
+    std::thread threads[NUMBER_OF_SERVOS];
+    for(int i = 0; i < NUMBER_OF_SERVOS; i++)
+    {
+        threads[i] = std::thread(moveServoToResistance, i, 0);
+    }
+    for(int i = 0; i < NUMBER_OF_SERVOS; i++)
+    {
+        threads[i].join();
+    }
+    return true;
+}
+
+bool openClaw()
+{
+    for(int i = 0; i < NUMBER_OF_SERVOS; i++)
+    {
+       moveServo(i, 90);
+    }
+    return true;
+}
+
 // runs the operations
 bool commandInput(String command)
 {
@@ -236,219 +159,23 @@ bool commandInput(String command)
     return false;
 }
 
-<<<<<<< HEAD
-// Define the VoltageReading class (formerly Point)
-class VoltageReading
+// setup stuff
+void setup()
 {
-public:
-    int voltage; // Storing the voltage value
+    Serial.begin(BAUD_RATE);
+    zero();
+}
 
-    VoltageReading(int voltage = 0) : voltage(voltage) {}
-
-    void display() const
-    {
-        std::cout << voltage;
-    }
-};
-
-// Define the VoltageReadingList class
-class VoltageReadingList
-{
-private:
-    std::vector<VoltageReading> readings; // The list of voltage readings
-    size_t max_size;                      // Maximum number of readings in the list
-
-public:
-    // Constructor to initialize with a specific number of readings
-    VoltageReadingList(size_t n) : max_size(n) {}
-
-    // Method to add a new voltage reading to the list
-    void addReading(const VoltageReading &new_reading)
-    {
-        if (readings.size() >= max_size)
-        {
-            // If the list is full, remove the first reading
-            readings.erase(readings.begin());
-        }
-        readings.push_back(new_reading); // Add the new voltage reading
-    }
-
-    // Method to display all voltage readings in the list
-    void displayReadings() const
-    {
-        for (const auto &reading : readings)
-        {
-            reading.display();
-            std::cout << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    // Method to get the current size of the list
-    size_t getSize() const
-    {
-        return readings.size();
-    }
-
-    // Method to check if the list size is valid
-    bool isValidSize() const
-    {
-        return readings.size() == max_size;
-    }
-
-    // Optionally, method to clear the list
-    void clear()
-    {
-        readings.clear();
-    }
-
-    // Method to calculate and return the average of the voltage readings
-    VoltageReading getAverage() const
-    {
-        if (readings.empty())
-        {
-            throw std::runtime_error("Cannot calculate average of an empty list.");
-        }
-
-        int sum = 0;
-        for (const auto &reading : readings)
-        {
-            sum += reading.voltage;
-        }
-
-        // Calculate average of voltage values
-        int avg_voltage = sum / readings.size();
-
-        return VoltageReading(avg_voltage);
-    }
-};
-
-int NUM_VOLTAGE_READINGS = 20;
-VoltageReadingList crab1 = VoltageReadingList(NUM_VOLTAGE_READINGS);
-VoltageReadingList crab2 = VoltageReadingList(NUM_VOLTAGE_READINGS);
-VoltageReadingList crab3 = VoltageReadingList(NUM_VOLTAGE_READINGS);
-=======
-int sensor_readings[10];
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
-
-// test runner
 void loop()
 {
-<<<<<<< HEAD
-    // while (Serial.available()) {
-    //     // krabbyPattyIndex: _
-    //     String input = Serial.readStringUntil('\n');
-
-    // }
-    // Serial.println(krabbyPattyLookUp[1].servo.read());
-    // krabbyPattyLookUp[0].servo.write(MIN_DEGREE);
-    int x = 50;
-    while (1 == 1)
-    {
-        krabbyPattyLookUp[0].servo.write(50);
-        Serial.println(krabbyPattyLookUp[0].servo.attached());
-        Serial.println(krabbyPattyLookUp[0].servo.read());
-
-        // Serial.println("working");
-        // for (int i = 0; i < 500; i++) {
-        //     // delay(10);
-        //     Serial.println(analogRead(SERVO_ONE_SENSOR));
-        // }
-        // servoOne.write(180);
-        // for (int i = 0; i < 500; i++) {
-        //     // delay(10);
-        //     Serial.println(analogRead(SERVO_ONE_SENSOR));
-        // }
-        // servoOne.write(0);
-        // for (int i = 0; i < 500; i++) {
-        //     // delay(10);
-        //     Serial.println(analogRead(SERVO_ONE_SENSOR));
-        // }
-=======
-    while (Serial.available()) {
+    if (Serial.available()) {
         commandInput(Serial.readString());
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
     }
-    // Serial.println(analogRead(krabbyPattyLookUp[0].sensor));
-    // int sensor_readout = kp.sensor.analogRead();
-    // Serial.println(krabbyPattyLookUp[0].sensor.analogRead());
-    // delay(5000);
-    while (1 == 1) {
-        // Serial.println(analogRead(krabbyPattyLookUp[0].sensor));
-        Serial.println(krabbyPattyLookUp[0].servo.read());
-        // Serial.println(krabbyPattyLookUp[0].servo.readTimerWidth());
-        moveServo(0, 50);
-    }
-    // delay(5000);
-    // moveServo(0, 100);
+    openClaw();
+    Serial.println("open");
+    delay(10000);
+    Serial.println("closing");
+    closeClaw();
+    Serial.println("closed");
+    delay(10000);
 }
-
-<<<<<<< HEAD
-const int NUM_TILL_CHECK = 5;
-
-// CHECK THE VALUES AND ADJUST THIS
-// no point translating to actual voltage values (unneccesary computations)
-const int NUM_THRESHOLD = 2000;
-
-// close servo
-// TODO: implement logic to run until ampMeter spike
-int closeServo(int servoOneAmt, int servoTwoAmt, int servoThreeAmt)
-{
-    int largestServo = largest(servoOneAmt, servoTwoAmt, servoThreeAmt);
-    int servoOneReadings[20];
-    int servoTwoReadings[20];
-    int servoThreeReadings[20];
-    // YASH ABSOLUTELY SUCKS
-    //  -LEO
-
-    // increment each servo by one up until closed
-    int i = 0;
-
-    bool stop1 = 0;
-    bool stop2 = 0;
-    bool stop3 = 0;
-
-    for (int i = 0; i < largestServo; i++)
-    {
-        // check if any have hit
-        if (i > NUM_VOLTAGE_READINGS && i == NUM_TILL_CHECK)
-        {
-            int avg1 = crab1.getAverage().voltage;
-            if (avg1 >= NUM_THRESHOLD)
-            {
-                stop1++;
-            }
-
-            int avg2 = crab2.getAverage().voltage;
-            if (avg2 >= NUM_THRESHOLD)
-            {
-                stop2++;
-            }
-
-            int avg3 = crab3.getAverage().voltage;
-            if (avg1 >= NUM_THRESHOLD)
-            {
-                stop3++;
-            }
-        }
-        crab1.addReading(analogRead(krabbyPattyLookUp[1].sensor));
-        crab2.addReading(analogRead(krabbyPattyLookUp[2].sensor));
-        crab3.addReading(analogRead(krabbyPattyLookUp[3].sensor));
-
-        if (krabbyPattyLookUp[1].location < servoOneAmt && stop1 < 4)
-        {
-            moveServo(1, 1);
-        }
-        if (krabbyPattyLookUp[2].location < servoTwoAmt && stop2 < 4)
-        {
-            moveServo(2, 1);
-        }
-        if (krabbyPattyLookUp[3].location < servoThreeAmt && stop3 < 4)
-        {
-            moveServo(3, 1);
-        }
-    }
-}
-=======
-#define sensorThreshold 300
->>>>>>> 71575a8bacd44338c4d6b9c98d99c7ccb499de34
