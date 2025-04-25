d84942f2263dae2632f9d7abf7f9b8db87f59ab2
Tanay Garg <tanay.garg@utexas.edu>
2025-04-25 12:00:58 -0500

    edited .gitignore and removed .pt

---
+++ clawArmPositionCalculator/ClawArm/.pio/build/esp32vn-iot-uno/FrameworkArduino/libb64/cdecode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32vn-iot-uno/FrameworkArduino/libb64/cdecode.c.o: \
+ /Users/leowit/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ /Users/leowit/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
---
+++ clawArmPositionCalculator/ClawArm/.pio/build/esp32vn-iot-uno/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32vn-iot-uno/FrameworkArduino/libb64/cencode.c.o: \
+ /Users/leowit/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ /Users/leowit/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
