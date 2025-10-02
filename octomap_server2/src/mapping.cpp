cbf4f63befbe1f2d63e21595ffdcdcf0242f5872
Tanay Garg <tanay.garg@utexas.edu>
2025-10-02 18:03:52 -0500

    octomap stuff added

--- octomap_server2/src/mapping.cpp
+++ octomap_server2/src/mapping.cpp
@@ -4,41 +4,92 @@
 #include <octomap/octomap.h>
 #include <octomap_msgs/msg/octomap.hpp>
 #include <octomap_msgs/conversions.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 
 class OctoMapping : public rclcpp::Node
 {
 public:
+    std::shared_ptr<octomap::OcTree> tree;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_;
+    rclcpp::Publisher<octomap_msgs::msg::Octomap>::SharedPtr pub_;
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    std::string target = "camera_depth_optical_frame";
+    std::string source = "link_base";
+
     OctoMapping()
-    : Node("octo_mapping")
+        : Node("octo_mapping"),
+          tf_buffer_(this->get_clock()),
+          tf_listener_(tf_buffer_)
     {
         tree = std::make_shared<octomap::OcTree>(0.1);
-
         sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            "/camera/camera/depth/camera_info", 10, std::bind(&OctoMapping::pointCloudCallback, this, std::placeholders::_1)
-        );
-        pub_ = this->create_publisher<octomap_msgs::msg::OctoMap>("/octomap", 10);
-    } 
+            "/camera/camera/depth/color/points", 10, std::bind(&OctoMapping::pointCloudCallback, this, std::placeholders::_1));
+
+        pub_ = this->create_publisher<octomap_msgs::msg::Octomap>("/octomap", 10);
+        
+        try
+        {
+            // Optional: wait until transform is available (non-throwing)
+            if (!tf_buffer_.canTransform(target, source, rclcpp::Time(0), tf2::durationFromSec(0.2)))
+            {
+                RCLCPP_WARN_THROTTLE(get_logger(), *get_clock(), 5000,
+                                     "Waiting for transform %s -> %s", target.c_str(), source.c_str());
+                return;
+            }
+        }
+        catch (const std::exception &e)
+        {
+            RCLCPP_WARN(this->get_logger(), "Exception while checking transforms: %s", e.what());
+        }
+    }
 
 private:
-    void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg){
+    void pointCloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+    {
+
+        std_msgs::msg::Header header = msg->header;
+
         std::vector<octomap::point3d> points;
-        for(sensor_msgs::PointCloud2ConstIterator<float> it_x(*msg, "x"), it_y(*msg, "y"), it_z(*msg, "z");
-            it_x != it_x.end();
-            ++it_x, ++it_y, ++it_z)
+        for (sensor_msgs::PointCloud2ConstIterator<float> it_x(*msg, "x"), it_y(*msg, "y"), it_z(*msg, "z");
+             it_x != it_x.end();
+             ++it_x, ++it_y, ++it_z)
         {
-            if(!std::isnan(*it_x) && !std::isnan(*it_y) && !std::isnan(*it_z)){
+            if (!std::isnan(*it_x) && !std::isnan(*it_y) && !std::isnan(*it_z))
+            {
                 points.emplace_back(*it_x, *it_y, *it_z);
             }
         }
 
-        if(points.empty()){
+        if (points.empty())
+        {
             RCLCPP_WARN(this->get_logger(), "No valid points in cloud");
             return;
         }
 
-        octomap::point3d origin(0.0f, 0.0f, 0.0f);
+        // Convert points to a octomap::PointCloud
+        octomap::Pointcloud pcl;
+        for (octomap::point3d point: points) {
+            pcl.push_back(point);
+        }
 
-        tree->insertPointCloud(points, origin);
+        octomap::point3d sensor_origin;
+        geometry_msgs::msg::TransformStamped tf =
+            tf_buffer_.lookupTransform(target, source, rclcpp::Time(0)); // latest
+
+        const auto &t = tf.transform.translation;
+        const auto &q = tf.transform.rotation;
+
+        RCLCPP_INFO(get_logger(),
+            "Pose of %s in %s: p = [%.3f, %.3f, %.3f], q = [%.3f, %.3f, %.3f, %.3f]",
+            source.c_str(), target.c_str(),
+            t.x, t.y, t.z, q.x, q.y, q.z, q.w);
+        octomap::pose6d origin(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+
+        tree->insertPointCloud(pcl, sensor_origin, origin);
 
         tree->writeBinary("octomap.bt");
         RCLCPP_INFO(this->get_logger(), "Octomap updated and saved to octomap.bt");
@@ -47,24 +98,26 @@ private:
         map_msg.header.stamp = this->get_clock()->now();
         map_msg.header.frame_id = "map";
 
-        if(octomap_msgs::fullMapToMsg(*tree_, map_msg)){
+        octomap_msgs::msg::Octomap oct_msg;
+        // oct_msg.data = octomap_msgs::fullMapToMsgData(tree, )
+
+        if (octomap_msgs::fullMapToMsg(*tree, map_msg))
+        {
             pub_->publish(map_msg);
             RCLCPP_INFO(this->get_logger(), "Published Octomap to /octomap");
-        } else {
+        }
+        else
+        {
             RCLCPP_WARN(this->get_logger(), "Failed to convert OcTree to Octomap");
         }
-        
     }
-
-    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr sub_;
-    rclcpp::Publisher<octomap_msgs::msg::OctoMap>::SharedPtr pub_;
-    std::shared_ptr<octomap::OcTree> tree_;
 };
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     rclcpp::init(argc, argv);
     auto node = std::make_shared<OctoMapping>();
     rclcpp::spin(node);
-    rclcpp:;shutdown();
+    rclcpp::shutdown();
     return 0;
 }
