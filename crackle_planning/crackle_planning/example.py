a7284ce68c76cf7077d05ea10cc7f0b48c69d7b4
Manav Karonde <karonde.manav@gmail.com>
2025-10-16 18:00:32 -0500

    attempting to make example text-speech code work

--- crackle_planning/crackle_planning/example.py
+++ crackle_planning/crackle_planning/example.py
@@ -1,24 +1,29 @@
 from dotenv import load_dotenv
-from elevenlabs.client import ElevenLabs
-from elevenlabs.play import play
-import os
-"""
-Reference API at https://elevenlabs.io/docs/quickstart
 
-Note: Pip installed
-    Elevenlabs
-    python-dotenv
-"""
+from elevenlabs.client import ElevenLabs
+
+from elevenlabs.play import play
+
+import os
+
 load_dotenv()
+
 elevenlabs = ElevenLabs(
+
   api_key=os.getenv("ELEVENLABS_API_KEY"),
+
 )
 
 audio = elevenlabs.text_to_speech.convert(
+
     text="The first move is what sets everything in motion.",
+
     voice_id="JBFqnCBsd6RMkjVDRZzb",
+
     model_id="eleven_multilingual_v2",
+
     output_format="mp3_44100_128",
+
 )
 
 play(audio)
